03cf7f128492ff96535871744ad7f67301e9e686
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-05-05 07:45:53 -0700

    Initial Commit

---
+++ ChatGPTRoom/ChatGPTRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTRoomApp.swift
+//  ChatGPTRoom
+//
+//  Created by Julian Worden on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
